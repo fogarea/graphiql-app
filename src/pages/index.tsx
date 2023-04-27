5b2f0ac074e27fa79e7f0bc70e9c44cddfaf3ccb
fogarea <shiitakedd@gmail.com>
2023-04-27 06:05:58 +0300

    init: start graphiql task

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { TestPage } from '@/pages/test';
+import { Route, Routes } from 'react-router-dom';
+
+export const Routing = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<TestPage />}>
+        <Route path="test" element={<TestPage />} />
+        <Route path="*" element={<TestPage />} />
+      </Route>
+    </Routes>
+  );
+};
---
+++ src/pages/test/index.tsx
@@ -0,0 +1,5 @@
+export const TestPage = (): JSX.Element => {
+  return (
+    <h1>Hello GraphiQL</h1>
+  );
+};
