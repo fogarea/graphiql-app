5ccae9110cc24ff4077e9deba7d409e92b622a58
kostili-tec <weh4teyou@gmail.com>
2023-05-01 23:13:39 +0300

    feat: add roboto font imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from '@/app';
 import { StyledEngineProvider } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
