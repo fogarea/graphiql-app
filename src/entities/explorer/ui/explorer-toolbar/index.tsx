73499aa3436626a1db6e25997e8942de8c18b696
Sapronov Konstantin <ks.costas@gmail.com>
2023-05-05 11:19:26 +0300

    fix: remove grid

--- src/entities/explorer/ui/explorer-toolbar/index.tsx
+++ src/entities/explorer/ui/explorer-toolbar/index.tsx
@@ -1,20 +1,11 @@
 import BookIcon from '@mui/icons-material/Book';
-import Grid from '@mui/material/Grid/Grid';
 import { TooltipButton } from '@/shared/ui';
 
 export const ExplorerToolbar = ({ toggleDocumentation }: IEditorToolbarProps): JSX.Element => {
   return (
-    <Grid
-      container
-      direction="column"
-      justifyContent="flex-start"
-      alignItems="center"
-      sx={{ width: '100%' }}
-    >
-      <TooltipButton title="Documentation" onClick={toggleDocumentation}>
-        <BookIcon />
-      </TooltipButton>
-    </Grid>
+    <TooltipButton title="Documentation" onClick={toggleDocumentation}>
+      <BookIcon />
+    </TooltipButton>
   );
 };
 
