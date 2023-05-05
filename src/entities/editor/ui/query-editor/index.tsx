15c86fcebfb9ae7c40e3b803aefb3e1c1fc348fe
Sapronov Konstantin <ks.costas@gmail.com>
2023-05-05 17:41:32 +0300

    feat: add locales for placeholder

--- src/entities/editor/ui/query-editor/index.tsx
+++ src/entities/editor/ui/query-editor/index.tsx
@@ -1,13 +1,16 @@
 import { Card } from '@mui/material';
 import CodeEditor from '@uiw/react-textarea-code-editor';
+import { useTranslation } from 'react-i18next';
 
 export const QueryEditor = ({ code, setCode }: IQueryEditorProps) => {
+  const { t } = useTranslation();
+
   return (
     <Card variant="outlined">
       <CodeEditor
         value={code}
         language="graphql"
-        placeholder="Please enter GraphQL query."
+        placeholder={t('editor.placeholder')}
         onChange={(evn) => setCode(evn.target.value)}
         padding={15}
         minHeight={360}
