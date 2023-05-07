09b8bf1b19124dba3e7ffcac8ef2ef20e1cfd7b2
Sapronov Konstantin <ks.costas@gmail.com>
2023-05-07 12:35:38 +0300

    fix: resolve pr comment

--- src/entities/editor/ui/index.ts
+++ src/entities/editor/ui/index.ts
@@ -1,7 +1,7 @@
 export { QueryEditor } from './query-editor';
 export { ResponseEditor } from './response-editor';
 export { EditorToolbar } from './editor-toolbar';
-export { HeaderEditor } from './heder-editor';
+export { HeaderEditor } from './header-editor';
 export { VariableEditor } from './variable-editor';
 export { EditorExpandMore } from './editor-expand-more';
 export { EditorTabPanel } from './tab-panel';
