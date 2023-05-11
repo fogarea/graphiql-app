135fdd29c6f3288a4da8652ead5c07d1bff443ca
fogarea <shiitakedd@gmail.com>
2023-05-11 18:15:10 +0300

    fix: replace persist by firebase auth listener

--- src/features/editor/editor-page-button/index.tsx
+++ src/features/editor/editor-page-button/index.tsx
@@ -2,15 +2,21 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+
+import { useAuth } from '@/entities/user';
 import { TypeAppRoute } from '@/shared/config';
 
 export const EditorPageButton = (): JSX.Element => {
   const { t } = useTranslation();
+  const { isAuth } = useAuth();
 
   return (
     <Grid item>
       <Button variant="contained">
-        <NavLink to={TypeAppRoute.Editor} style={{ textDecoration: 'none', color: 'inherit' }}>
+        <NavLink
+          to={isAuth ? TypeAppRoute.Editor : TypeAppRoute.Login}
+          style={{ textDecoration: 'none', color: 'inherit' }}
+        >
           {t('editor.welcome-page')}
         </NavLink>
       </Button>
