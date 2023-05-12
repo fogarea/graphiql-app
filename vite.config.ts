65d386be01e4de3cd3c895f844496c8c78762565
Sapronov Konstantin <ks.costas@gmail.com>
2023-05-12 10:44:09 +0300

    feat: add rollup plugin visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import autoprefixer from 'autoprefixer';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export const pathResolver = (p: string) => resolve(__dirname, '.', p);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   css: {
     postcss: {
       plugins: [autoprefixer],
