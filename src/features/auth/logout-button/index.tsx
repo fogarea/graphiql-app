a69b29f12f7193ad62b86de1c666b14bd6a54148
fogarea <shiitakedd@gmail.com>
2023-05-13 23:15:52 +0300

    fix: logout button style

--- src/features/auth/logout-button/index.tsx
+++ src/features/auth/logout-button/index.tsx
@@ -1,22 +1,21 @@
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
 import { useNavigate } from 'react-router-dom';
-
 import { useTranslation } from 'react-i18next';
+
 import { useAuth } from '@/entities/user';
+import { DefaultButton } from '@/shared/ui';
 import { TypeAppRoute } from '@/shared/config';
 
 export const LogoutButton = (): JSX.Element => {
   const { t } = useTranslation();
-
   const { logout } = useAuth();
-
   const navigate = useNavigate();
+
   return (
-    <Grid item>
-      <Button color="inherit" onClick={() => void logout(() => navigate(TypeAppRoute.Logout))}>
-        {t('editor.log-out')}
-      </Button>
-    </Grid>
+    <DefaultButton
+      variant="contained"
+      onClick={() => void logout(() => navigate(TypeAppRoute.Logout))}
+    >
+      {t('editor.log-out')}
+    </DefaultButton>
   );
 };
