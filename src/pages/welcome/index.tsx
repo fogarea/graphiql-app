e32fa298f371d7cfe896039b352de36bec121dc1
fogarea <shiitakedd@gmail.com>
2023-05-14 13:27:00 +0300

    refactor: compose developers and school info  into about-us widget

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,29 +1,11 @@
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
-import { useTranslation } from 'react-i18next';
-
 import { WelcomeHeroLayout } from '@/widgets/welcome-hero';
-import { Developers } from '@/widgets/developers';
+import { AboutUs } from '@/widgets/about-us';
 
 export const WelcomePage = (): JSX.Element => {
-  const { t } = useTranslation();
-
   return (
     <>
       <WelcomeHeroLayout />
-      <Developers />
-      <Container>
-        <Grid container gap={1} justifyContent="center" mt={10} mb={10}>
-          <Typography variant="subtitle2">
-            {t('welcome.course-info1')}
-            <Link href="https://rs.school/react/">React Course</Link>
-            {t('welcome.course-info2')}
-            <Link href="https://rs.school/index.html">RS School</Link>
-          </Typography>
-        </Grid>
-      </Container>
+      <AboutUs />
     </>
   );
 };
--- src/widgets/about-us/ui/index.tsx
+++ src/widgets/about-us/ui/index.tsx
@@ -1,6 +1,7 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
 import { useTranslation } from 'react-i18next';
 
 import { DeveloperCard, developerService } from '@/entities/developer';
@@ -8,7 +9,7 @@ import { a11yHeader2TextAlign } from '@/shared/theme';
 
 const developers = developerService.getAll();
 
-export const Developers = () => {
+export const AboutUs = () => {
   const { t } = useTranslation();
 
   return (
@@ -21,6 +22,15 @@ export const Developers = () => {
         {developers.map((developer) => (
           <DeveloperCard key={developer.githubName} {...developer} />
         ))}
+      </Grid>
+
+      <Grid container justifyContent="center" mt={10} mb={10}>
+        <Typography variant="subtitle2">
+          {t('welcome.course-info1')}
+          <Link href="https://rs.school/react/">React Course</Link>
+          {t('welcome.course-info2')}
+          <Link href="https://rs.school/index.html">RS School</Link>
+        </Typography>
       </Grid>
     </Container>
   );
