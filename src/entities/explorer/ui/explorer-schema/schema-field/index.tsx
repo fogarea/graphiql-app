393bfbf32d2d609e869eb42f46d41ae7776106db
kostili-tec <weh4teyou@gmail.com>
2023-05-15 17:18:15 +0300

    feat: add type fields with info. add colors

--- src/entities/explorer/ui/explorer-schema/schema-field/index.tsx
+++ src/entities/explorer/ui/explorer-schema/schema-field/index.tsx
@@ -1,13 +1,36 @@
 import { ParserField } from 'graphql-js-tree';
 import Grid from '@mui/material/Grid';
 
+import styles from './styles.module.scss';
+
 export const SchemaField = (props: ParserField) => {
+  const { type, args } = props;
   return (
     <Grid container>
       <pre>
-        <span>{props.type.fieldType.type}</span>
+        <span className={styles.type}>{type.fieldType.name}</span>
+        {/*  // проблема интерфейса, name существует */}
         <span> </span>
-        <span>{props.name}</span>
+        <span className={styles.typeName}>{props.name}</span>
+        <span>{` {`}</span>
+        <pre style={{ margin: 0 }}>
+          {args.length &&
+            args.map((typeField) => (
+              <pre style={{ margin: 5 }} key={typeField.id}>
+                <span className={styles.typeField}>{`  ${typeField.name}:`}</span>
+                {typeField.type.fieldType.name ? (
+                  <span
+                    className={styles.typeFieldType}
+                  >{` ${typeField.type.fieldType.name}`}</span>
+                ) : (
+                  <span
+                    className={styles.typeFieldType}
+                  >{` [${typeField.type.fieldType.nest.name}]`}</span>
+                )}
+              </pre>
+            ))}
+        </pre>
+        <span>{`}`}</span>
       </pre>
     </Grid>
   );
