a119875849d52c09c1eb6340a0306ca51781e1b2
fogarea <shiitakedd@gmail.com>
2023-05-19 17:15:42 +0300

    feat: implement page title

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -1,8 +1,14 @@
 import Container from '@mui/material/Container';
+import { useTranslation } from 'react-i18next';
 
 import { GraphQLEditor } from '@/widgets/graphql-editor';
+import { useTitle } from '@/shared/lib';
 
 export const EditorPage = (): JSX.Element => {
+  const { t } = useTranslation();
+
+  useTitle(t('page-title.editor'));
+
   return (
     <Container maxWidth="xl">
       <GraphQLEditor />
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,11 +1,16 @@
 import { Navigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 import { RegisterContainer } from '@/features/auth';
 import { useAuth } from '@/entities/user';
 import { TypeAppRoute } from '@/shared/config';
+import { useTitle } from '@/shared/lib';
 
 export const RegisterPage = (): JSX.Element => {
+  const { t } = useTranslation();
   const { isAuth } = useAuth();
+
+  useTitle(t('page-title.register'));
 
   if (isAuth) {
     return <Navigate to={TypeAppRoute.Editor} replace />;
