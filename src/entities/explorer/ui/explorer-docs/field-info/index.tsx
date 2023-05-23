21339ae3da65382fde401c999c86bd1d231722f0
kostili-tec <weh4teyou@gmail.com>
2023-05-23 14:06:13 +0300

    feat: show full field info on click type details

--- src/entities/explorer/ui/explorer-docs/field-info/index.tsx
+++ src/entities/explorer/ui/explorer-docs/field-info/index.tsx
@@ -1,14 +1,53 @@
-import { IDocsTypeDetails, getQueryValue } from '@/entities/explorer';
+import { useEffect, useState } from 'react';
+
+import {
+  IDocsTypeDetails,
+  ITypeArguments,
+  getQueryInfo,
+  getTypeDetails,
+  useExplorer,
+} from '@/entities/explorer';
 
 export const ExplorerFieldInfo = ({ typeDetails }: IDocsTypeDetails): JSX.Element => {
-  const typeValue = getQueryValue(typeDetails);
+  const { parsedSchema } = useExplorer();
+  const [docsDetails, setDocsDetails] = useState<ITypeArguments[]>([]);
+  const queryInfo = getQueryInfo(typeDetails);
+  console.log('queryInfo', queryInfo);
+
+  useEffect(() => {
+    const findQuery = parsedSchema.find((el) => el.name === queryInfo.name);
+    console.log('findQuery', findQuery);
+    if (findQuery) {
+      const details = getTypeDetails(findQuery);
+      const isValidDetails = details?.every((el): el is ITypeArguments => {
+        return el !== undefined && typeof el.name === 'string' && typeof el.type === 'string';
+      });
+      if (isValidDetails) setDocsDetails(details);
+    } else {
+      setDocsDetails([]);
+    }
+  }, [queryInfo.name, parsedSchema]);
+
   return (
     <div style={{ width: 300 }}>
       <pre>
         <span>{typeDetails.name}: </span>
-        <span>{typeValue}</span>
+        <span>{queryInfo.name}</span>
       </pre>
       <p>{typeDetails.description}</p>
+      {!!docsDetails.length && (
+        <div>
+          <span>{'type '}</span>
+          <span>{'{'}</span>
+          {docsDetails.map((el) => (
+            <pre key={el.name}>
+              <span>{el.name}: </span>
+              <span>{el.type}</span>
+            </pre>
+          ))}
+          <span>{'}'}</span>
+        </div>
+      )}
     </div>
   );
 };
