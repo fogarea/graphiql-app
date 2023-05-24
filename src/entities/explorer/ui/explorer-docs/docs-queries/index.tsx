afadc4475c73df425b77bb95db181c02fe7d1098
kostili-tec <weh4teyou@gmail.com>
2023-05-25 01:04:41 +0300

    refactor: delete comments

--- src/entities/explorer/ui/explorer-docs/docs-queries/index.tsx
+++ src/entities/explorer/ui/explorer-docs/docs-queries/index.tsx
@@ -1,4 +1,3 @@
-// import { useTranslation } from 'react-i18next';
 import { Divider } from '@mui/material';
 
 import {
@@ -8,13 +7,11 @@ import {
 import { DrawerHeader } from '../../explorer-drawer/drawer-header';
 
 export const ExplorerDocsQueries = ({ parsedSchema }: IExplorerDocsQueriesProps): JSX.Element => {
-  // const { t } = useTranslation();
   const schemaQueries = parsedSchema.find((field) => field.name === docsQuery.Query);
 
   return (
     <div style={{ width: 270, paddingTop: 6, display: 'flex', flexDirection: 'column' }}>
       <DrawerHeader>
-        {/* <h2>{t('explorer.documentation')}</h2> */}
         <h2>QUERIES</h2>
       </DrawerHeader>
       <Divider sx={{ marginBottom: 1 }} />
