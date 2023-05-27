62fdf03874f4427b277e2e112baf15244463a1d5
kostili-tec <weh4teyou@gmail.com>
2023-05-27 19:23:33 +0300

    feat: docs container has become adaptive

--- src/entities/explorer/ui/explorer-drawer/index.tsx
+++ src/entities/explorer/ui/explorer-drawer/index.tsx
@@ -1,5 +1,6 @@
 import Container from '@mui/material/Container/Container';
 import Drawer from '@mui/material/Drawer/Drawer';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { ReactNode } from 'react';
 
 export const ExplorerDrawer = ({
@@ -7,9 +8,21 @@ export const ExplorerDrawer = ({
   children,
   toggleDrawer,
 }: IExplorerDrawerProps): JSX.Element => {
+  const matches = useMediaQuery('(min-width:890px)');
+  const flexDirection = matches ? 'row' : 'column';
+  const gapSize = matches ? 0 : 1;
+  console.log(matches);
   return (
     <Drawer anchor="right" open={open} onClose={toggleDrawer} hideBackdrop={false}>
-      <Container sx={{ paddingTop: 2, paddingBottom: 2, display: 'flex', flexDirection: 'row' }}>
+      <Container
+        sx={{
+          paddingTop: 2,
+          paddingBottom: 2,
+          display: 'flex',
+          flexDirection: flexDirection,
+          gap: gapSize,
+        }}
+      >
         {children}
       </Container>
     </Drawer>
