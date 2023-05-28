1aa6792bfd95e5fc22aa5bb938f66ca49baf6696
kostili-tec <weh4teyou@gmail.com>
2023-05-28 19:48:44 +0300

    refactor: delete commented code

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -1,21 +1,12 @@
-// import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { GraphQLEditor } from '@/widgets/graphql-editor';
-// import { useExplorer } from '@/entities/explorer';
 import { useTitle } from '@/shared/lib/browser';
 
 export const EditorPage = (): JSX.Element => {
   const { t } = useTranslation();
-  // const { isLoaded, parsedSchema, execSchema } = useExplorer();
 
   useTitle(t('page-title.editor'));
-
-  /*  useEffect(() => {
-    if (!isLoaded || !parsedSchema) {
-      execSchema();
-    }
-  }, [isLoaded, parsedSchema, execSchema]); */
 
   return <GraphQLEditor />;
 };
