a6fc0b3cb7535e2962f779375c8106638571fdbb
fogarea <shiitakedd@gmail.com>
2023-06-04 00:27:53 +0300

    fix: add try catch for explorer parser helper

--- src/entities/explorer/lib/helpers.ts
+++ src/entities/explorer/lib/helpers.ts
@@ -2,10 +2,15 @@ import { buildClientSchema, IntrospectionQuery, printSchema } from 'graphql';
 import { Parser } from 'graphql-js-tree';
 
 export const parseResultsSchema = (results: string) => {
-  const data = JSON.parse(results) as IntrospectionQuery;
-  const schema = buildClientSchema(data);
-  const printedSchema = printSchema(schema);
-  const parsedSchema = Parser.parse(printedSchema);
+  try {
+    const data = JSON.parse(results) as IntrospectionQuery;
+    const schema = buildClientSchema(data);
+    const printedSchema = printSchema(schema);
 
-  return parsedSchema;
+    return Parser.parse(printedSchema);
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message);
+    }
+  }
 };
