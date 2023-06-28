c5906a65a192c7e11273f665e560cf9b636ef8ca
fogarea <shiitakedd@gmail.com>
2023-06-28 18:58:52 +0300

    feat: implement styles for endpoints

--- src/entities/editor/ui/editor-endpoint/index.tsx
+++ src/entities/editor/ui/editor-endpoint/index.tsx
@@ -2,17 +2,12 @@ import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import { GRAPHQL_API_ENDPOINT } from '@/shared/config';
+import { a11yEditorEndpointFormControl } from '@/shared/lib/theme';
 
 export const EditorEndpoint = (): JSX.Element => {
   return (
-    <FormControl size="small" sx={{ my: 2, minWidth: 80, maxWidth: { lg: '100%', xs: 288 } }}>
-      <Select
-        value=""
-        autoWidth
-        displayEmpty
-        inputProps={{ 'aria-label': 'Without label' }}
-        disabled
-      >
+    <FormControl size="small" sx={{ ...a11yEditorEndpointFormControl() }}>
+      <Select value="" autoWidth displayEmpty disabled>
         <MenuItem value="">{GRAPHQL_API_ENDPOINT}</MenuItem>
       </Select>
     </FormControl>
